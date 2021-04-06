46a00baeb789cf6e63493950d64b6cb5144cabe9
Dominik Krone (SE-A/34) <Dominik.krone@online.de>
2021-04-06 14:03:57 +0200

    sayHello_myName_rtnHelloName_passed

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,14 +2,18 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello World";
+    public String sayHello(@RequestParam(defaultValue="World", required = false) String name){
+
+        return String.format("<h1>hello %s<h1/>", name);
     }
+
+
 
 }
--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest (HelloController.class)
 public class HelloControllerTests {
@@ -18,10 +20,17 @@ public class HelloControllerTests {
         //hello String "hello World"
 
         //act
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+        mockMvc.perform(get("/hello"))
 
         //assert
-        .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+        .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    void sayHello_myName_rtnHelloName() throws Exception {
+        mockMvc.perform(get("/hello?name=Dominik"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("<h1>hello Dominik<h1/>"));
     }
 }
